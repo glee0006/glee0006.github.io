d16dce601ae1f5bd7dd74dcecefb21991bd48e79
glee0006 <leleglee420@gmail.com>
2024-10-16 16:34:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function downloadData() {
   event.preventDefault();
   var startdate = document.getElementById("startdate").value;
   var enddate = document.getElementById("enddate").value;
-  concole.log(startdate);
+  concole.log (startdate);
   console.log(enddate);
 }
